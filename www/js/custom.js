0e0e0179baa331158a1fdea274a43246d2e08944
JustinXyrel <justinxyreldorongon@gmail.com>
2017-01-27 11:09:44 +0800

    change toolbar

--- www/js/custom.js
+++ www/js/custom.js
@@ -2,7 +2,7 @@
             base_url ='http://www.pointonesolutions.com.ph/naac/';
             function new_address(url){
 
-                var ref = cordova.InAppBrowser.open(url, '_blank', 'fullscreen=yes,closebuttoncaption=Home');
+                var ref = cordova.InAppBrowser.open(url, '_blank', 'location=no,fullscreen=yes,closebuttoncaption=Home');
                     ref.addEventListener('loadstart', function(event) { alert(event.url); });
                //  document.addEventListener("deviceready", onDeviceReady, false);
                //  function onDeviceReady() {
